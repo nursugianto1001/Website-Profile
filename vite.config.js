b1d69004c9655b02f6b879bb7cd20c9e85fb4535
Muhammad Ilham Zulfaishal <77421455+OniSakadev@users.noreply.github.com>
2025-06-11 20:43:11 +0700

    testing deploy (zul)
    hanya untuk uji coba pendeployan

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 
+                'resources/js/bootstrap.js',
+                'resources/images/open.jpg',
+                'resources/images/servis.jpg',
+                'resources/images/Karvin.png',
+                'resources/images/tim 2.jpg',
+                'resources/images/salam-karvin.jpg',
+                'resources/images/map.png',
+                'resources/js/app.js',
+            ], 
             refresh: true,
         }),
     ],
